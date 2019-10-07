11b2908755867f1713529d223cde672dea0fb63d
Michael Di Spirito <55723271+michaeldispirito@users.noreply.github.com>
2019-10-06 21:44:14 -0700

    Add files via upload
    - Added commenting functionality
    - Added timer functionality

--- RedditAutoCommenter.py
+++ RedditAutoCommenter.py
@@ -1,27 +1,65 @@
 import praw
 import time
+import datetime
 
-reddit = praw.Reddit(client_id='f2Pcn3B31HsMuA',
-                     client_secret='ImENvEwV0wjpJwPV72dwWZ3wIJo',
-                     password='BananaApple',
-                     user_agent='script:MusicFeedbackCommenter:v1.0 (by /u/MusicTest1234)',
-                     username='MusicTest1234')
 
-#print(reddit.user.me())
+targetSubreddit = 'test'
+commentToPost = 'Test Comment'
+authorOfPost = 'MusicTest1234'
+titleOfPost = 'test number 10'
+user = 'user'
+
+daysAhead = 1
+delay = 0
+
+def createComment(post, time):
+    post.reply(commentToPost)
+    print('Comment Successfully Created at {}:{}:{}'.format(time.hour, time.minute, time.second))
 
 def searchSubreddit(subreddit):
     global postFound
-    for submission in subreddit.new(limit=1):
-        print('searching')
-        if (submission.author == 'MusicTest1234') and (submission.title =="myTest"):
+    searchTime = datetime.datetime.now()
+    print('Searching at {}:{}:{}'.format(searchTime.hour, searchTime.minute, searchTime.second))
+    for submission in subreddit.new(limit=10):
+        if (submission.author == authorOfPost) and (submission.title == titleOfPost):
              postFound = True
-             print(submission.title)
-             print(submission.author)
-        time.sleep(5)
-   
+             createComment(submission, searchTime)
+             break
+    time.sleep(2)
         
-subreddit = reddit.subreddit('test')
+def getTimeToSearch():
+    global delay
+    global daysAhead
+    curTime = datetime.datetime.now()
+
+    if (curTime.weekday == 0 or curTime.weekday == 4):
+        daysAhead = 0
+    
+    targetTime = datetime.datetime.today() + datetime.timedelta(days = daysAhead)
+    targetTime = targetTime.replace(hour = 3, minute = 0, second = 0, microsecond = 0)
+    
+    delay = targetTime - curTime
+    delay = delay.total_seconds()
+
+#Connect to Reddit
+reddit = praw.Reddit(client_id='abcdef',
+                     client_secret='secret',
+                     password='pass',
+                     user_agent='script:WATMMFeedbackCommenter:v1.0 (by /u/user)',
+                     username= user)
+
+#Check if connection is made
+if (reddit.user.me() == user):
+    print('Connected to Reddit')
+
+#Search the subreddit
+subredditToSearch = reddit.subreddit(targetSubreddit)
 postFound = False
 
+getTimeToSearch()
+print('Time until 3am in seconds = {}'.format(delay))
+
+#time.sleep(30)
+
 while (not postFound):
-    searchSubreddit(subreddit)
+    searchSubreddit(subredditToSearch)
